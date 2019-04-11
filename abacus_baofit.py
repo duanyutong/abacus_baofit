fixed sign error in particle radial speed after taking dot product projecting onto the radial direction

abacus_baofit.py
@@ -37,15 +37,15 @@ from baofit import baofit
 
 # %% custom settings
 
-#sim_name_prefix = 'emulator_1100box_planck'
-#tagout = '-mmatter'  # 'norsd'  # '# 'matter'  # 'z0.5'
-#phases = range(16)  # range(16)  # [0, 1] # list(range(16))
-#sim_name_prefix = 'AbacusCosmos_1100box_planck'
-#tagout = '-mmatter'
-#phases = range(20)
-sim_name_prefix = 'joint_1100box_planck'
-tagout = 'mmatter'
-phases = range(36)
+# sim_name_prefix = 'emulator_1100box_planck'
+# tagout = '-recon'  # 'norsd'  # '# 'matter'  # 'z0.5'
+# phases = range(16) # range(16)  # [0, 1] # list(range(16))
+sim_name_prefix = 'AbacusCosmos_1100box_planck'
+tagout = ''
+phases = range(20)
+# sim_name_prefix = 'joint_1100box_planck'
+# tagout = ''
+# phases = range(36)
 
 model_names = ['gen_base1', 'gen_base6', 'gen_base7',
                'gen_ass1',  'gen_ass1_n',
@@ -54,13 +54,13 @@ model_names = ['gen_base1', 'gen_base6', 'gen_base7',
                'gen_s1',    'gen_s1_n',
                'gen_sv1',   'gen_sv1_n',
                'gen_sp1',   'gen_sp1_n']
-model_names = ['matter']
-reals = range(1)  # range(12)
+model_names = ['gen_base6', 'gen_base7']
+reals = range(12)
 N_threads = 24  # number of threads for a single realisation
-do_create = False
-do_coadd = False
+do_create = True
+do_coadd = True
 do_optimise_fitter = False
-do_fit = True
+do_fit = False
 
 cosmology = 0  # one cosmology at a time instead of 'all'
 redshift = 0.5  # one redshift at a time instead of 'all'
@@ -74,7 +74,7 @@ matter_subsample_fraction = 0.002
 
 # %% flags
 save_hod_realisation = True
-use_rsd = False
+use_rsd = True
 
 # %% bin settings
 step_s_bins = 5  # mpc/h, bins for fitting
@@ -884,12 +884,12 @@ def do_realisation(r, phase, model_name, overwrite=False,
         raise E
 
 
-recon_types = ['pre-recon', 'post-recon-std']  # 'post-recon'ite'
+recon_types = ['pre-recon', 'post-recon-std', 'post-recon-ite']
 coadd_filenames = [  # coadd_realisations, change this for iterative recon
-#    '-auto-xi-smu-pre-recon-ar',
-#    '-auto-xi_0-smu-pre-recon-ar', '-auto-xi_2-smu-pre-recon-ar',
-#    '-auto-xi-rppi-pre-recon-ar', '-auto-xi-rppi-post-recon-std-sr',
-#    '-auto-wp-rppi-pre-recon-ar', '-auto-wp-rppi-post-recon-std-sr',
+    '-auto-xi-smu-pre-recon-ar',
+    '-auto-xi_0-smu-pre-recon-ar', '-auto-xi_2-smu-pre-recon-ar',
+    '-auto-xi-rppi-pre-recon-ar', '-auto-xi-rppi-post-recon-std-sr',
+    '-auto-wp-rppi-pre-recon-ar', '-auto-wp-rppi-post-recon-std-sr',
 ]  # exclude iterative reconstruction for fitter test with baseline HOD
 for recon_type in recon_types:
     coadd_filenames = coadd_filenames + [
@@ -1110,7 +1110,7 @@ def combine_galaxy_table_metadata(phases):
 class FitterCache:
 
     def __init__(self, model_name, recon_type, force_isotropy=False,
-                 beta=0, rmin=50, r_rescale_factor=1):
+                 beta=0, rmin=50, r_rescale_factor=1, jk=True):
         '''
         input parameters are: (beta, r_rescale_factor)
         '''
@@ -1119,12 +1119,16 @@ class FitterCache:
         self.model_name = model_name
         self.recon_type = recon_type
         self.force_isotropy = force_isotropy
+        self.jk = jk
         self.results = {}  # initialise lookup dict results[beta][r_fac]
-        self.baofit_parallel((beta, 1))  # pass beta prime
+        print('model_name', model_name)
         if 'matter' in model_name:
             self.sample_type = 'mat'
+            print('setting sample type mat')
         else:
             self.sample_type = 'gal'
+            print('setting sample type gal')
+        self.baofit_parallel((beta, 1))
 
     def baofit_parallel(self, parameters):
         # input beta and r_rescale factor (algways ignored)
@@ -1148,25 +1152,54 @@ class FitterCache:
         alpha = {}
         alpha['mat'] = {'r': np.zeros(len(phases)),
                         't': np.zeros(len(phases))}
-        for phase in phases:  # list of inputs for parallelisation
-            # sim_name = '{}_{:02}-{}'.format(
-            #         sim_name_prefix, cosmology, phase)
-            path_p_lin = "/mnt/gosling1/bigsim_products/" \
-                "AbacusCosmos_1100box_planck_products/" \
-                "AbacusCosmos_1100box_planck_00-0_products/" \
-                "AbacusCosmos_1100box_planck_00-0_power/" \
-                "info/camb_matterpower.dat"
-            # path_p_lin = os.path.join(
-            #     prod_dir, sim_name+'_products', sim_name+'_power', 'info',
-            #     'camb_matterpower.dat')
+        path_p_lin = "/mnt/gosling1/bigsim_products/" \
+            "AbacusCosmos_1100box_planck_products/" \
+            "AbacusCosmos_1100box_planck_00-0_products/" \
+            "AbacusCosmos_1100box_planck_00-0_power/" \
+            "info/camb_matterpower.dat"
+        # path_p_lin = os.path.join(
+        #     prod_dir, sim_name+'_products', sim_name+'_power',
+        #     'info', 'camb_matterpower.dat')
+        if self.jk:  # all jackknife samples for all phases
+            for phase in phases:  # list of inputs for parallelisation
+                # sim_name = '{}_{:02}-{}'.format(
+                #         sim_name_prefix, cosmology, phase)
+                path_xi_0 = os.path.join(
+                    indir,
+                    '{}-auto-fftcorr_xi_0-{}-jackknife_{}-coadd.txt'
+                    .format(self.model_name, xi_type, phase))
+                path_xi_2 = os.path.join(
+                    indir,
+                    '{}-auto-fftcorr_xi_2-{}-jackknife_{}-coadd.txt'
+                    .format(self.model_name, xi_type, phase))
+                if 'pre-recon' in xi_type:
+                    recon = False
+                    path_cov = os.path.join(
+                        indir,
+                        '{}-cross-xi_monoquad-cov-smu-pre-recon-ar.txt'
+                        .format(self.model_name))
+                elif 'post-recon' in xi_type:
+                    recon = True
+                    path_cov = os.path.join(
+                        indir,
+                        '{}-cross-xi_monoquad-cov-smu-post-recon-std-sr.txt'
+                        .format(self.model_name))
+                fout_tag = '{}-{}-{}'.format(self.model_name, phase, xi_type)
+                path_cg = os.path.join(outdir, fout_tag+'-chisq_grid.txt')
+                path_ct = os.path.join(outdir, fout_tag+'-chisq_table.txt')
+                list_of_inputs.append(
+                    [self.sample_type, redshift, recon, self.force_isotropy,
+                     self.polydeg, self.rmin, beta, path_p_lin,
+                     path_xi_0, path_xi_2, path_cov, path_cg, path_ct])
+        else:
             path_xi_0 = os.path.join(
                 indir,
-                '{}-auto-fftcorr_xi_0-{}-jackknife_{}-coadd.txt'
-                .format(self.model_name, xi_type, phase))
+                '{}-auto-fftcorr_xi_0-{}-coadd.txt'
+                .format(self.model_name, xi_type))
             path_xi_2 = os.path.join(
                 indir,
-                '{}-auto-fftcorr_xi_2-{}-jackknife_{}-coadd.txt'
-                .format(self.model_name, xi_type, phase))
+                '{}-auto-fftcorr_xi_2-{}-coadd.txt'
+                .format(self.model_name, xi_type))
             if 'pre-recon' in xi_type:
                 recon = False
                 path_cov = os.path.join(
@@ -1179,7 +1212,7 @@ class FitterCache:
                     indir,
                     '{}-cross-xi_monoquad-cov-smu-post-recon-std-sr.txt'
                     .format(self.model_name))
-            fout_tag = '{}-{}-{}'.format(self.model_name, phase, xi_type)
+            fout_tag = '{}-{}'.format(self.model_name, xi_type)
             path_cg = os.path.join(outdir, fout_tag+'-chisq_grid.txt')
             path_ct = os.path.join(outdir, fout_tag+'-chisq_table.txt')
             list_of_inputs.append(
@@ -1251,6 +1284,7 @@ if __name__ == "__main__":
                 halocat = make_halocat(phase, halo_type='FoF')
                 print('\nSkipping halocat processing, matter field...')
             else:
+                halocat = make_halocat(phase, halo_type='Rockstar')
                 halocat = process_rockstar_halocat(halocat, N_cut=N_cut)
             for model_name in model_names:
                 print('---\nWorking on {} realisations, phase {}, model {}...'
@@ -1259,7 +1293,7 @@ if __name__ == "__main__":
                                     maxtasksperchild=1)) as p:
                     p.map(partial(do_galaxy_table,
                                   phase=phase, model_name=model_name,
-                                  overwrite=False),
+                                  overwrite=True),
                           reals)
                     p.close()
                     p.join()
@@ -1267,13 +1301,13 @@ if __name__ == "__main__":
                                     maxtasksperchild=1)) as p:
                     p.map(partial(
                             do_realisation, phase=phase, model_name=model_name,
-                            overwrite=False,
-                            do_pre_auto_smu=False, do_pre_auto_rppi=False,
+                            overwrite=True,
+                            do_pre_auto_smu=True, do_pre_auto_rppi=True,
                             do_pre_auto_fft=True, do_pre_cross=True,
-                            do_recon_std=False, do_post_auto_rppi=False,
-                            do_post_cross=False, do_recon_ite=False,
-                            do_pre_auto_corr=False, do_post_auto_corr=False,
-                            do_pre_cross_corr=True, do_post_cross_corr=False),
+                            do_recon_std=True, do_post_auto_rppi=True,
+                            do_post_cross=True, do_recon_ite=True,
+                            do_pre_auto_corr=True, do_post_auto_corr=True,
+                            do_pre_cross_corr=True, do_post_cross_corr=True),
                           reals)
                     p.close()
                     p.join()
@@ -1298,26 +1332,27 @@ if __name__ == "__main__":
         optimise_fitter('gen_base1', 'pre-recon', brute=True)
     if do_fit:
         for model_name in model_names:
-            # for recon in recon_types:
-            '''
-            best-fit parameters for 16-box sim post-recon:
-            real-space matter: beta = 0.00, r_rescale_factor = 1.00174
-            redshift-space ga: beta = 0.00, r_rescale_factor = 1.00232
+            recon_types = ['post-recon-std', 'post-recon-ite']
+            for recon in recon_types:
+                '''
+                best-fit parameters for 16-box sim post-recon:
+                real-space matter: beta = 0.00, r_rescale_factor = 1.00174
+                redshift-space ga: beta = 0.00, r_rescale_factor = 1.00232
 
-            best-fit parameters for 20-box sim post-recon:
-            real-space matter: beta = 0.00, r_rescale_factor = 0.99843
-            redshift-space ga: beta = 0.00, r_rescale_factor = 0.99707
+                best-fit parameters for 20-box sim post-recon:
+                real-space matter: beta = 0.00, r_rescale_factor = 0.99843
+                redshift-space ga: beta = 0.00, r_rescale_factor = 0.99707
 
-            we expect a 0.2% shift due to nonlinear evolutions when fitting
-            with a linear power spectrum which does not account for
-            nonlinear effects; no need to fiddle with r-rescaling
+                we expect a 0.2% shift due to nonlinear evolutions when fitting
+                with a linear power spectrum which does not account for
+                nonlinear effects; no need to fiddle with r-rescaling
 
-            best-fit parameters for 36-box joint set:
-            real-space matter pre-/post-recon: beta = 0.00
-            redshift-space galaxy pre-recon:   beta = 0.00
-            (lower chisq 50 vs 80, a insensitive to beta to 0.2%, )
-            redshift-space galaxy post-recon:  beta = 0.00
-            '''
-            # model_name = 'gen_base1'
-            cache = FitterCache(model_name, 'pre-recon',
-                                force_isotropy=False, beta=0, rmin=50)
+                best-fit parameters for 36-box joint set:
+                real-space matter pre-/post-recon: beta = 0.00
+                redshift-space galaxy pre-recon:   beta = 0.00
+                (lower chisq 50 vs 80, a insensitive to beta to 0.2%, )
+                redshift-space galaxy post-recon:  beta = 0.00
+                '''
+                # model_name = 'gen_base1'
+                cache = FitterCache(model_name, recon, jk=False,
+                                    force_isotropy=False, beta=0, rmin=50)

abacus_hod.py
@@ -334,15 +334,16 @@ def process_particle_props(pt, h, halo_m_prop='halo_mvir', perihelion=False,
     pt['vx_pec'] = pt['vx'] - pt['halo_vx']
     pt['vy_pec'] = pt['vy'] - pt['halo_vy']
     pt['vz_pec'] = pt['vz'] - pt['halo_vz']
-    v_pec = np.array([pt['vx_pec'], pt['vy_pec'], pt['vz_pec']]).T
+    v_pec = np.array([pt['vx_pec'], pt['vy_pec'], pt['vz_pec']]).T  # vector
     # for some particles close to halo centre due to single precision, we find
     # v_pec = v_rad = v_tan = 0, and r_0 = r_min
     pt['v_pec'] = np.linalg.norm(v_pec, axis=1)  # peculiar speed km/s
     # dimensionless r relative hat
     norm = np.linalg.norm(r_rel, axis=1)
     norm = norm.reshape(len(norm), 1)  # reshape it into a column vector
-    r_rel_hat = r_rel / norm
-    pt['v_rad'] = np.sum(np.multiply(v_pec, r_rel_hat), axis=1)  # dot prod
+    r_rel_hat = r_rel / norm  # unit vector in the relative/radial direction
+    # dot product (radial component of peculiar speed) could be negative
+    pt['v_rad'] = np.abs(np.sum(np.multiply(pt['v_pec'] , r_rel_hat), axis=1))
     pt['v_tan'] = np.sqrt(np.square(pt['v_pec']) - np.square(pt['v_rad']))
     # calculate perihelion distance for bias s_p, note h factors
     if perihelion:
@@ -586,10 +587,10 @@ def initialise_model(redshift, model_name, halo_m_prop='halo_m'):
             model.param_dict['logM1'] = 13.805
             model.param_dict['alpha'] = 1.05
         elif model_name == 'gen_base6':
-            model.param_dict['logM1'] = 13.793  # tune this
+            model.param_dict['logM1'] = 13.770
             model.param_dict['alpha'] = 0.75
         elif model_name == 'gen_base7':
-            model.param_dict['logM1'] = 13.829  # tune this
+            model.param_dict['logM1'] = 13.848
             model.param_dict['alpha'] = 1.25
         elif model_name == 'gen_ass1':
             model.param_dict['A_cen'] = 1
@@ -839,7 +840,7 @@ def make_galaxies(model):
         if model.param_dict['s_p'] != 0:
             pt['rank_s_p'][m:m+n] = pt['r_perihelion'][m:m+n] \
                 .argsort()[::-1].argsort()
-
+    print('r = {:2d}, calculating new probabilities...'.format(model.r))
     # calculate new probability regardless of decoration parameters
     # if any s is zero, the formulae guarantee the random numbers are unchanged
     # only re-rank halos where subsamp_len >= 2

baofit.py
@@ -49,7 +49,7 @@ class baofit3D:
             # Sigma_para = 4  # Mpc/h
         else:  # pre-recon peak is wider
             if self.sample_type == 'mat':
-                Sigma_perp = 2  # 2 Mpc/h for matter, 5 Mpc/h for galaxies
+                Sigma_perp = 1.5  # 1.5 Mpc/h for matter, 5 Mpc/h for galaxies
             elif self.sample_type == 'gal':
                 Sigma_perp = 5
             # Sigma_para = 10  # Mpc/h
@@ -103,10 +103,10 @@ class baofit3D:
         self.B2_ln_width = 100
         self.A = np.zeros(6)
         self.range_ok = False
-        print(('\nReconstructed: {}, force isotropy: {}, '
+        print(('\nReconstructed: {}, sample: {}, force isotropy: {}, '
                'xitemp rescale factor: {:.2f}, data vector shape: {}')
-              .format(reconstructed, force_isotropy, xi_temp_rescale,
-                      self.dv.shape))
+              .format(reconstructed, sample_type, force_isotropy,
+                      xi_temp_rescale, self.dv.shape))
 
     def xi_r_mu(self, r, mu, ell_max=6):
         ''' input r may be 2D, return 2D xi_r_mu(r, mu) '''
@@ -197,35 +197,35 @@ class baofit3D:
     def chisq_scan(self):
         '''
         matter pre-recon:
-                ars = np.arange(1.0060, 1.0120, 0.00005)
-                ats = np.arange(0.9970, 1.0020, 0.00005)
+                ars = np.arange(1.0065, 1.0110, 0.00005)
+                ats = np.arange(0.9985, 1.0020, 0.00005)
 
         matter post-recon:
-                ars = np.arange(0.9960, 1.0000, 0.00005)
-                ats = np.arange(0.9990, 1.0010, 0.00005)
+                ars = np.arange(0.9960, 0.9990, 0.00005)
+                ats = np.arange(0.9995, 1.0020, 0.00005)
 
         galaxy pre-recon:
-                ars = np.arange(0.9980, 1.0170, 0.0001)
-                ats = np.arange(1.0020, 1.0190, 0.0001)
+                ars = np.arange(0.990, 1.016, 0.0001)
+                ats = np.arange(0.993, 1.016, 0.0001)
 
         galaxy post-recon:
-                ars = np.arange(0.9900, 1.0040, 0.0001)
-                ats = np.arange(0.9950, 1.0005, 0.0001)
+                ars = np.arange(0.990, 1.016, 0.0001)
+                ats = np.arange(0.993, 1.016, 0.0001)
         '''
         if self.sample_type == 'mat':
             if not self.reconstructed:  # pre-recon
-                ars = np.arange(1.0060, 1.0120, 0.00005)
-                ats = np.arange(0.9970, 1.0020, 0.00005)
+                ars = np.arange(1.0065, 1.0110, 0.00005)
+                ats = np.arange(0.9985, 1.0020, 0.00005)
             else:  # post=recon
-                ars = np.arange(0.9960, 1.0000, 0.00005)
-                ats = np.arange(0.9990, 1.0010, 0.00005)
+                ars = np.arange(0.9960, 0.9990, 0.00005)
+                ats = np.arange(0.9995, 1.0020, 0.00005)
         elif self.sample_type == 'gal':
             if not self.reconstructed:  # pre-recon
-                ars = np.arange(0.9980, 1.0170, 0.0001)
-                ats = np.arange(1.0020, 1.0190, 0.0001)
+                ars = np.arange(0.990, 1.016, 0.0001)
+                ats = np.arange(0.993, 1.016, 0.0001)
             else:  # post-recon
-                ars = np.arange(0.9900, 1.0040, 0.0001)
-                ats = np.arange(0.9950, 1.0005, 0.0001)
+                ars = np.arange(0.990, 1.016, 0.0001)
+                ats = np.arange(0.993, 1.016, 0.0001)
         chisq_grid = np.zeros((ars.size, ats.size))  # default value is 0
         B0_grid = np.zeros(chisq_grid.shape)
         B2_grid = np.zeros(chisq_grid.shape)
@@ -260,8 +260,11 @@ class baofit3D:
         self.at = at
         self.xitemp = self.make_xi_temp()
         chisq = self.B0_B2_optimize((B0, B2))  # this sets A hat coefficients
+        dxi = self.dv - self.mv
+        print('dxi_0:', np.mean(dxi[:self.n]),
+              'dxi_2:', np.mean(dxi[self.n:]), 'chisq', chisq)  # debug
         assert chisq == chisq_min
-        r_full = np.arange(1, 151, 1)  # debug
+        r_full = np.arange(2.5, 150, 5)  # np.arange(1, 151, 1)  # debug
         self.xitemp = self.make_xi_temp(r0=r_full)
         ximod0, ximod2 = self.make_model_vector(B0, B2, r0=r_full)
         return np.array([r_full, ximod0, ximod2,
@@ -278,6 +281,7 @@ def baofit(argv):
                 os.mkdir(os.path.dirname(path_cg))
             except FileExistsError:
                 pass
+        print('inputs are:\n', argv)
         data = np.loadtxt(path_p_lin)
         k = data[:, 0]
         P_lin = data[:, 1]
@@ -332,7 +336,8 @@ def baofit(argv):
         np.savetxt(path_cg.replace('chisq_grid.txt', 'xi_0_2_model.txt'),
                    model_dump, fmt=txtfmt)
         ar, at, chisq = chisq_table[np.argmin(chisq_table[:, 2]), :]
-        print('\nBest-fit ar, at, chisq: ({:.4f}, {:.4f}), {:.2f}, range OK:'
+        print(('\nBest-fit ar, at, chisq: ({:.4f}, {:.4f}), {:.2f}, '
+               'range OK: {}')
               .format(ar, at, chisq, fit.range_ok))
         return ar, at
     except Exception as E: